a36b1c7b94bb2cf09e6276b855c24609f395c413
mark-mit-k <warneke.mark@gmail.com>
2016-11-15 19:02:55 +0100

    finalized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,95 +6,129 @@
 
 (function($) {
 
-	skel
-		.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+    skel
+        .breakpoints({
+            xlarge: '(max-width: 1680px)',
+            large: '(max-width: 1280px)',
+            medium: '(max-width: 980px)',
+            small: '(max-width: 736px)',
+            xsmall: '(max-width: 480px)'
+        });
 
-	$(function() {
+    $(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#page-wrapper'),
-			$banner = $('#banner'),
-			$header = $('#header');
+        var $window = $(window),
+            $body = $('body'),
+            $wrapper = $('#page-wrapper'),
+            $banner = $('#banner'),
+            $header = $('#header');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                $body.removeClass('is-loading');
+            }, 100);
+            
 
-		// Mobile?
-			if (skel.vars.mobile)
-				$body.addClass('is-mobile');
-			else
-				skel
-					.on('-medium !medium', function() {
-						$body.removeClass('is-mobile');
-					})
-					.on('+medium', function() {
-						$body.addClass('is-mobile');
-					});
+             $('#three').poptrox({
+                caption: function($a) {
+                    return $a.next('h3').text(); },
+                overlayColor: '#2c2c2c',
+                overlayOpacity: 0.85,
+                popupCloserText: '',
+                popupLoaderText: '',
+                selector: '.ref-item a.image',
+                usePopupCaption: true,
+                usePopupDefaultStyling: false,
+                usePopupEasyClose: false,
+                usePopupNav: true,
+                windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+            });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+              $('#five').poptrox({
+                caption: function($a) {
+                    return $a.next('h3').text(); },
+                overlayColor: '#2c2c2c',
+                overlayOpacity: 0.85,
+                popupCloserText: '',
+                popupLoaderText: '',
+                selector: '.ag-item a.image',
+                usePopupCaption: true,
+                usePopupDefaultStyling: false,
+                usePopupEasyClose: false,
+                usePopupNav: true,
+                windowMargin: (skel.breakpoint('small').active ? 0 : 50)
+            });
+        });
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Mobile?
+        if (skel.vars.mobile)
+            $body.addClass('is-mobile');
+        else
+            skel
+            .on('-medium !medium', function() {
+                $body.removeClass('is-mobile');
+            })
+            .on('+medium', function() {
+                $body.addClass('is-mobile');
+            });
 
-		// Scrolly.
-			$('.scrolly')
-				.scrolly({
-					speed: 1500,
-					offset: $header.outerHeight()
-				});
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// Menu.
-			$('#menu')
-				.append('<a href="#menu" class="close"></a>')
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-menu-visible'
-				});
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function() {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
 
-		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
+        // Scrolly.
+        $('.scrolly')
+            .scrolly({
+                speed: 1500,
+                offset: $header.outerHeight()
+            });
 
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
+        // Menu.
+        $('#menu')
+            .append('<a href="#menu" class="close"></a>')
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'right',
+                target: $body,
+                visibleClass: 'is-menu-visible'
+            });
 
-				$window.on('resize', function() { $window.trigger('scroll'); });
+        // Header.
+        if (skel.vars.IEVersion < 9)
+            $header.removeClass('alt');
 
-				$banner.scrollex({
-					bottom:		$header.outerHeight() + 1,
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); }
-				});
+        if ($banner.length > 0 && $header.hasClass('alt')) {
 
-			}
+            $window.on('resize', function() { $window.trigger('scroll'); });
 
-	});
+            $banner.scrollex({
+                bottom: $header.outerHeight() + 1,
+                terminate: function() { $header.removeClass('alt'); },
+                enter: function() { $header.addClass('alt'); },
+                leave: function() { $header.removeClass('alt'); }
+            });
+
+        }
+
+
+
+    });
+
+
 
 })(jQuery);
